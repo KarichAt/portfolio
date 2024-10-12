523a6be8453fd55a578c96221d6a38d0accd6f4a
KarichAt <ilyushonok2005@gmail.com>
2024-10-12 14:27:42 +0300

    element luse

---
+++ src/components/footer/footer.tsx
@@ -0,0 +1,10 @@
+import '../footer/footer.css'
+export default function Footer(){
+    return (
+        <>
+        <footer className="footer">
+                <span className="mess">- Hi! ^. .^</span>
+        </footer>
+        </>
+    )
+}
