45318912c8f8207249f4ef908941820362aa20fb
KarichAt <ilyushonok2005@gmail.com>
2024-10-12 18:43:23 +0300

    deployer

--- src/main.js
+++
@@ -1,8 +0,0 @@
-import {HashRouter} from 'react-router-dom';
-const NewApp = (props) => {
-  return  <HashRouter basename={process.env.PUBLIC_URL}>
-      <Provider store={store}>
-          <AppContainer />
-      </Provider>
-    </HashRouter>
- }
